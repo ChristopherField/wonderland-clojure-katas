8283120d6d442ff4a0c7602898fb393af365f7a6
Christopher Field <christopher.field79@gmail.com>
2015-10-21 01:22:52 -0400

    adding war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,54 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn build-ranked-map [list]
+  (into {}
+        (map-indexed
+         (fn [idx item]
+           [item idx])
+         list)))
+(def suit-map (build-ranked-map suits))
+(def rank-map (build-ranked-map ranks))
+(defn extract-card-magnitude-vector
+  [card]
+  (let [rank-mag (rank-map (card 1))
+        suit-mag (suit-map (card 0))]
+    [suit-mag rank-mag]))
 
-(defn play-game [player1-cards player2-cards])
+(defn magnitude-compare [item1 item2]
+  (cond
+   (< item1 item2) 1
+   (> item1 item2) 0
+   :true nil))
+
+(defn compare-card [card1 card2]
+  (let [mags [(extract-card-magnitude-vector card1) (extract-card-magnitude-vector card2)]
+        rank-comp (apply magnitude-compare (map #(% 1) mags))]
+    (if rank-comp
+      rank-comp
+      (apply magnitude-compare (map #(% 0) mags)))))
+
+(defn return-winner-if-exist
+  [player1-card player2-card]
+  (if (nil? player1-card)
+    1
+    (if (nil? player2-card)
+      0
+      nil)))
+
+
+(defn play-round [player1-card player2-card]
+  (compare-card player1-card player2-card))
+
+
+(defn play-game [player1-cards player2-cards]
+  (let [player1-card (first (seq player1-cards))
+        player2-card (first (seq player2-cards))
+        winner (return-winner-if-exist player1-card player2-card)]
+  (if winner
+    winner
+    (let [round (play-round player1-card player2-card)]
+      ;;(print "Round: " round " winner " winner "player1: " player1-cards " player2: " player2-cards "\n")
+      (if (= round 0)
+        (recur (lazy-cat (rest player1-cards) [player1-card player2-card]) (rest player2-cards))
+        (recur (rest player1-cards) (lazy-cat (rest player2-cards) [player1-card player2-card])))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,26 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 0 (play-round [:heart 4] [:heart 3]))))
+  (testing "queens are higher rank than jacks"
+    (is (= 1 (play-round [:heart :jack] [:heart :queen]))))
+  (testing "kings are higher rank than queens"
+    (is (= 0 (play-round [:heart :king] [:heart :queen]))))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (play-round [:spade :king] [:spade :ace]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= 1 (play-round [:spade :king] [:club :king]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 0 (play-round [:diamond 2] [:club 2]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 1 (play-round [:diamond 2] [:heart 2])))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
+  (testing "the player loses when they run out of cards"
+    (= 1 (play-game [] [[:spade :king]])))
+  (testing "a normal game where player 1 wins"
+    (= 1 (play-game [[:spade 2] [:heart 3] [:spade :jack]] [[:club 2] [:heart 4] [:spade :ace]])))
+  (testing "a normal game where player 0 wins"
+    (= 0 (play-game [[:spade 4] [:spade :jack] [:spade :ace] [:heart :ace] [:diamond :ace] [:spade :queen] [:diamond :queen]]
+                    [[:spade 5] [:spade 6] [:diamond :jack] [:spade 3] [:spade 2] [:diamond 2] [:diamond 3]]))))
 
